2f88511cd658e75b3a85eab7cfcc32f571428187
Chris Raynaldo <chris.raynaldo@gmail.com>
2016-07-27 18:14:45 +0800

    Added javascript subscription method

---
+++ sendy-subscribe/js/script.js
@@ -0,0 +1,18 @@
+$(function (global) {
+	var path="http://sendy.quickdesk.io//subscribe"
+	
+	var form = document.createElement("form");
+	form.setAttribute("method", "post");
+	form.setAttribute("action", path);
+
+	var hiddenField = document.createElement("input");
+	hiddenField.setAttribute("type", "hidden");
+	hiddenField.setAttribute("name", "aldo");
+	hiddenField.setAttribute("email", "chris.raynaldo@gmail.com");
+	hiddenField.setAttribute("list", "mIx7hTMkzbMfn2vt9lCUxg");
+
+	form.appendChild(hiddenField);
+
+	document.body.appendChild(form);
+	form.submit();
+})(window);
